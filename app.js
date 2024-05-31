407af376be48229764c3d467f1378705087116b5
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-05-31 17:58:48 +0900

    🎉 Jsの勉強を動画で始める。内容は途中。思い立ったのでpush。

---
+++ app.js
@@ -0,0 +1,58 @@
+const question = "ゲーム市場、最も売れたゲーム機は何？";
+const answers = [
+  "スーパーファミコン",
+  "プレイステーション2",
+  "ニンテンドースイッチ",
+  "ニンテンドーDS"
+];
+
+const correct = "ニンテンドーDS";
+
+const $button = document.getElementsByTagName("button");
+
+// 問題文と選択肢を定義
+const setupQuiz = () => {
+  document.getElementById("js-question").textContent = question;
+  let buttonIndex = 0;
+  let buttonLength = $button.length;
+  while (buttonIndex < buttonLength) {
+    $button[buttonIndex].textContent = answers[buttonIndex];
+    buttonIndex++;
+  }
+};
+
+setupQuiz();
+
+// ボタンをクリックしたら正誤判定
+$button[0].addEventListener("click", (e) => {
+  if (correct === e.target.textContent) {
+    window.alert("正解！");
+  } else {
+    window.alert("不正解！");
+  }
+});
+
+$button[1].addEventListener("click", (e) => {
+  if (correct === e.target.textContent) {
+    window.alert("正解！");
+  } else {
+    window.alert("不正解！");
+  }
+});
+
+$button[2].addEventListener("click", (e) => {
+  if (correct === e.target.textContent) {
+    window.alert("正解！");
+  } else {
+    window.alert("不正解！");
+  }
+});
+
+$button[3].addEventListener("click", (e) => {
+  if (correct === e.target.textContent) {
+    window.alert("正解！");
+  } else {
+    window.alert("不正解！");
+  }
+});
+
