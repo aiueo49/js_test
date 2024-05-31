258f66ece1760df0b2dec19d1557e937f73e0285
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-05-31 20:41:29 +0900

    🚧 スタイルを操作

--- tab.js
+++ tab.js
@@ -2,8 +2,13 @@
 
   const $doc = document;
   const $tab = $doc.getElementById('js-tab');
+  const $nav = $tab.querySelectorAll('[data-nav]');
+  const $content = $tab.querySelectorAll('[data-content]');
 
-
+  const init = () => {
+    $content[0].style.display = 'block';
+  };
+  init();
 
 
 
