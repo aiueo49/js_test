9c7a362e237287632bbf78b5abce53d7b5cf8840
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-05-31 21:47:44 +0900

    ♻️ 繰り返すワードを定数化

--- tab.js
+++ tab.js
@@ -4,6 +4,8 @@
   const $tab = $doc.getElementById('js-tab');
   const $nav = $tab.querySelectorAll('[data-nav]');
   const $content = $tab.querySelectorAll('[data-content]');
+  const ACTIVE_CLASS = 'is-active';
+  const navLen = $nav.length;
 
   const init = () => {
     $content[0].style.display = 'block';
@@ -20,21 +22,21 @@
 
     // 対象外のnav, content全て一旦リセットする
     let index = 0;
-    while(index < $nav.length) {
+    while(index < navLen) {
       $content[index].style.display = 'none';
-      $nav[index].classList.remove('is-active');
+      $nav[index].classList.remove(ACTIVE_CLASS);
       index++;
     }
 
     // 対象コンテンツをアクティブ化する
     $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
-    $nav[targetVal].classList.add('is-active');
+    $nav[targetVal].classList.add(ACTIVE_CLASS);
 
 
   };
 
   let index = 0;
-  while(index < $nav.length) {
+  while(index < navLen) {
     $nav[index].addEventListener('click', (e) => handleClick(e));
     index++;
   }
