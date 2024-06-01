9ac069602211d1254ea7bd65bb54391923b167f4
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-06-01 10:10:40 +0900

    ♻️ 問題文と回答を定義

--- 2/app.js
+++ 2/app.js
@@ -7,14 +7,19 @@ const answers = [
 ];
 const correct = 'ニンテンドーDS';
 
-document.getElementById('js-question').textContent = question;
-
 const $button = document.getElementsByTagName('button');
 
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
+// 問題文と選択肢を定義
+const setuoQuiz = () => {
+  document.getElementById('js-question').textContent = question;
+  let buttonIndex = 0;
+  let buttonLength = $button.length;
+  while (buttonIndex < buttonLength) {
+    $button[buttonIndex].textContent = answers[buttonIndex];
+    buttonIndex++;
+  }
+}
+setuoQuiz();
 
 // ボタンをクリックしたら正誤判定
 $button[0].addEventListener('click', () => {
