853b3ae0eb151beb10027ea690a9fe5aa342a0a4
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-06-01 11:31:55 +0900

    🚧 3問動かす

--- 2/app.js
+++ 2/app.js
@@ -1,31 +1,63 @@
-const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？';
-const answers = [
-  'スーパーファミコン',
-  'プレイステーション2',
-  'ニンテンドースイッチ',
-  'ニンテンドーDS'
+const quiz = [
+  {
+    question: 'ゲーム市場、最も売れたゲーム機は次のうちどれ？',
+    answers: [
+      'スーパーファミコン',
+      'プレイステーション2',
+      'ニンテンドースイッチ',
+      'ニンテンドーDS'
+    ],
+    correct: 'ニンテンドーDS'
+  }, {
+    question: '任天堂の看板ゲームといえば？',
+    answers: [
+      'スーパーマリオ',
+      'ドンキーコング',
+      'ポケモン',
+      'ゼルダの伝説'
+    ],
+    correct: 'スーパーマリオ'
+  }, {
+    question: 'FFⅦの主人公は？',
+    answers: [
+      'クラウド',
+      'セフィロス',
+      'ティファ',
+      'エアリス'
+    ],
+    correct: 'クラウド'
+  }
 ];
-const correct = 'ニンテンドーDS';
+const quizLength = quiz.length;
+let quizIndex = 0;
 
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 
 // 問題文と選択肢を定義
-const setuoQuiz = () => {
-  document.getElementById('js-question').textContent = question;
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
   let buttonIndex = 0;
   while (buttonIndex < buttonLength) {
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 }
-setuoQuiz();
+setupQuiz();
 
 const clickHandler = (e) => {
-  if (correct === e.target.textContent) {
+  if (quiz[quizIndex].correct === e.target.textContent) {
     window.alert('正解！');
   } else {
     window.alert('不正解！');
+  }
+
+  quizIndex++;
+
+  if (quizIndex < quizLength) {
+    setupQuiz();
+  } else {
+    window.alert('終了！');
   }
 }
 
