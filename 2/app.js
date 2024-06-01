b546dc020f0c576d3a0cb370c3a80d9f97547675
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-06-01 10:04:08 +0900

    ✨ 1問完成

---
+++ 2/app.js
@@ -0,0 +1,47 @@
+const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？';
+const answers = [
+  'スーパーファミコン',
+  'プレイステーション2',
+  'ニンテンドースイッチ',
+  'ニンテンドーDS'
+];
+const correct = 'ニンテンドーDS';
+
+document.getElementById('js-question').textContent = question;
+
+const $button = document.getElementsByTagName('button');
+
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
+
+// ボタンをクリックしたら正誤判定
+$button[0].addEventListener('click', () => {
+  if (correct === document.getElementsByTagName('button')[0].textContent) {
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+});
+$button[1].addEventListener('click', () => {
+  if (correct === document.getElementsByTagName('button')[1].textContent) {
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+});
+$button[2].addEventListener('click', () => {
+  if (correct === document.getElementsByTagName('button')[2].textContent) {
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+});
+$button[3].addEventListener('click', () => {
+  if (correct === document.getElementsByTagName('button')[3].textContent) {
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+});
